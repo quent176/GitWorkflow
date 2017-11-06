34d0ae6570b30c787fd369833e84b94e022715e1
quent176 <quentin7611@yahoo.fr>
2017-11-06 10:58:37 +0100

    Add Googlemaps API

--- app/src/main/java/fr/wcs/gitworkflow/MainActivity.java
+++ app/src/main/java/fr/wcs/gitworkflow/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wcs.gitworkflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_goToMaps = (Button) findViewById(R.id.button_gotomaps);
+
+        button_goToMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+
     }
 }
